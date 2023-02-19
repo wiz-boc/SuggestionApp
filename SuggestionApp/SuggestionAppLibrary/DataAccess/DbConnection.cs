16b8751bb61f180dace2b54ae42fe2bca9775f7d
wiz-boc <wiz_kidz@outlook.com>
2023-02-19 00:14:27 -0500

    Setup Dbconnection class

--- SuggestionApp/SuggestionAppLibrary/DataAccess/DbConnection.cs
+++ SuggestionApp/SuggestionAppLibrary/DataAccess/DbConnection.cs
@@ -1,7 +1,33 @@
 ﻿using System;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+
 namespace SuggestionAppLibrary.DataAccess;
 
 public class DbConnection
 {
+    private readonly IConfiguration _config;
+    private readonly IMongoDatabase _db;
+    private string _connectionId = "MongoDB";
+    public string DbName { get; private set; }
+    public string CategoryCollectionName { get; private set; } = "categories";
+    public string StatusCollectionName { get; private set; } = "statues";
+    public string UserCollectionName { get; private set; } = "users";
+    public string SuggestionCollectionName { get; private set; } = "suggestions";
+    
+    public MongoClient Client { get; private set; }
+    
+    public IMongoCollection<CategoryModel> CategoryCollection { get; private set; }
+    public IMongoCollection<StatusModel> StatusCollection { get; private set; }
+    public IMongoCollection<UserModel> UserCollection { get; private set; }
+    public IMongoCollection<SuggestionModel> SuggestionCollection { get; private set; }
+
+    public DbConnection(IConfiguration config)
+    {
+        _config = config;
+        Client = new MongoClient(_config.GetConnectionString(_connectionId));
+        DbName = _config["DatabaseName"];
+        _db = Client.GetDatabase(DbName);
+    }
 }
 
