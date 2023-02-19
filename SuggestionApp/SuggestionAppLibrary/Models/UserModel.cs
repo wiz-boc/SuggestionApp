6cd64b0590c8e71ace83161972c1d70e78afd5f0
wiz-boc <wiz_kidz@outlook.com>
2023-02-19 18:39:06 -0500

    Creating Suggestion Data access methods

--- SuggestionApp/SuggestionAppLibrary/Models/UserModel.cs
+++ SuggestionApp/SuggestionAppLibrary/Models/UserModel.cs
@@ -11,7 +11,7 @@ public class UserModel
     public string LastName { get; set; }
     public string DisplayName { get; set; }
     public string EmailAddress { get; set; }
-    public List<SuggestionModel> AuthoredSuggestions { get; set; } = new();
-    public List<SuggestionModel> VotedOnSuggestions { get; set; } = new();
+    public List<BasicSuggestionModel> AuthoredSuggestions { get; set; } = new();
+    public List<BasicSuggestionModel> VotedOnSuggestions { get; set; } = new();
 }
 
--- SuggestionApp/SuggestionAppUI/RegisterServices.cs
+++ SuggestionApp/SuggestionAppUI/RegisterServices.cs
@@ -10,6 +10,12 @@ public static class RegisterServices
         builder.Services.AddRazorPages();
         builder.Services.AddServerSideBlazor();
         builder.Services.AddMemoryCache();
+
+        builder.Services.AddSingleton<IDbConnection, DbConnection>();
+        builder.Services.AddSingleton<ICategoryData, MongoCategoryData>();
+        builder.Services.AddSingleton<IStatusData, MongoStatusData>();
+        builder.Services.AddSingleton<ISuggestionData, MongoSuggestionData>();
+        builder.Services.AddSingleton<IUserData, MongoUserData>();
     }
 }
 
